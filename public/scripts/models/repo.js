410c5f97744a463c126b00ef0a559047d0ebb0c8
yanil3500 <yanil3500@gmail.com>
2017-02-22 16:25:51 -0800

    Ready to deploy

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -6,12 +6,8 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
+    // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
+    $.get('github/user/repos')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
